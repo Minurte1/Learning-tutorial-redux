084cfb281ca9497a5f7b33dad53dbf3548682bb8
Minurte1 <hohoangphucjob@gmail.com>
2024-09-05 22:25:22 +0700

    #14 useSelector, useDispatch Redux Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { connect } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./action/actions";
 import store from "./redux/store";
 function App(props) {
+  const dispatch = useDispatch();
+  const newCount = useSelector((state) => {
+    return state.counter123.count;
+  });
   const handleIncrease = () => {
-    props.increaseCounter1();
+    dispatch(increaseCounter());
+    // props.increaseCounter1();
     // store.dispatch({
     //   type: "test hoi dan it ",
     //   payload: { name: "eric" },
     // });
   };
   const handleDecrease = () => {
-    props.decreaseCounter();
+    dispatch(decreaseCounter());
+    // props.decreaseCounter();
   };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Learning react and redux !</h1> <div>Count: {props.abc}</div>
+        <h1>Learning react and redux !</h1> <div>Count: {newCount}</div>
         <p>{props.countHoiDanIT}</p>
         <button onClick={() => handleIncrease()}>Increase Count</button>
         <button onClick={() => handleDecrease()}>Decrease Count</button>
@@ -29,24 +35,26 @@ function App(props) {
 }
 
 // map state (redux + store) + props react
-const mapStateToProps = (state) => {
-  return {
-    countHoiDanIT: state.counter123.count,
-    abc: state.counter123.name,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     countHoiDanIT: state.counter123.count,
+//     abc: state.counter123.name,
+//   };
+// };
 
 //event.target.value
 
-//map dispatch to props react
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increaseCounter1: () => dispatch(increaseCounter()),
+// //map dispatch to props react
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increaseCounter1: () => dispatch(increaseCounter()),
 
-    decreaseCounter: () => dispatch(decreaseCounter()),
-  };
-};
+//     decreaseCounter: () => dispatch(decreaseCounter()),
+//   };
+// };
 
-//higher order component
-//cấy 2 hàm mapStateToProps mapDispatchToProps vào APP
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// //higher order component
+// //cấy 2 hàm mapStateToProps mapDispatchToProps vào APP
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default App;
